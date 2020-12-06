c11f82f011d9ea8670ec0a350a3b796955f357ca
Cermit <sintregal@gmail.com>
2020-12-05 22:02:58 -0500

    Solved 1.2

---
+++ 1/1.2.py
@@ -0,0 +1,19 @@
+#!python
+from os import path as ospath
+from sys import path as syspath
+
+with open(ospath.join(syspath[0], 'input'), 'r') as file:
+    inputdata = [int(line.strip()) for line in file]
+inputdata.sort()
+
+
+def answer():
+    for num1 in inputdata:
+        diff1 = 2020 - num1
+        for num2 in inputdata:
+            num3 = diff1 - num2
+            if num3 in inputdata:
+                return num1 * num2 * num3
+
+
+print(answer())
