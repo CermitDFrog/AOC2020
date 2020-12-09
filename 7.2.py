351ec04d41c82a3ce07069b6deaa7b6ca8cfd448
Cermit <sintregal@gmail.com>
2020-12-08 20:54:53 -0500

    Updated for 7.2

--- 7.2.py
+++ 7.2.py
@@ -1,7 +1,33 @@
 #!python3
 from helpers import iter_test, iter_input
 
-input_data = []
+"shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags."
+
+input_data = {}
 for item in iter_input(7):
-    print(item)
-    input()
+    key, rules = item.split("s contain ")
+    input_data[key] = {}
+    if "no other bags" not in rules:
+        for rule in rules.split(', '):
+            rule = rule.replace('bags', 'bag').replace('.', '')
+            num = rule.split()[0]
+            bag = " ".join(rule.split()[1:])
+            input_data[key][bag] = num
+
+
+def solveSevenTwo():
+    containers = ["shiny gold bag"]
+
+    def getContainers():
+        global containers
+        for bagtype in containers:
+            for bag, number in input_data[bagtype].items():
+                for x in range(int(number)):
+                    containers.append(bag)
+
+    getContainers()
+    containers.pop(0)
+    return len(containers)
+
+
+print(solveSevenTwo())
